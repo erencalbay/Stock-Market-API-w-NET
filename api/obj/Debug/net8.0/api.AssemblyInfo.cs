368961c05a8bfcc918714f5b02951d195bd97396
Eren Çalbay <calbayeren@gmail.com>
2024-08-13 17:19:03 +0300

    Feat: Models

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a24fe9d841ad77bdc40f9b1457e14c7fe97b1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386dbdc70accf101904cdf8ef6bda683515a8962")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
