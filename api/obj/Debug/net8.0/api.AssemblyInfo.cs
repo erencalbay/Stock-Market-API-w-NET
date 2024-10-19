148ef1bc9ec74d9d115d137d02a4027c4cebdadc
Eren Çalbay <calbayeren@gmail.com>
2024-10-19 17:22:17 +0300

    Feat: Identity

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+772518bc04df42e6ff0e7b9eab914fa86272805b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd097419ceaa59cb808a101479e8fdc025b712b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
