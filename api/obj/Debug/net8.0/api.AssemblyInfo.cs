b8ac57ffe252f55e4c08322b3260ce205e85fda7
Eren Çalbay <calbayeren@gmail.com>
2024-10-21 17:50:35 +0300

    Feat: Login API

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a22bbd9cf6be41acb51585fb98e088c78adaa758")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a799f428049a496e3c0d6f29bd2a23b82199f14")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
